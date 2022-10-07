53efb39f53752def1cefd40ce2e3c8a5baafdd10
Maksim Vikhrov <maxvihr@yandex.ru>
2022-10-07 18:18:13 +0500

    Step3: Add generate_diff

---
+++ gendiff/generate_diff.py
@@ -0,0 +1,39 @@
+import json
+
+
+def make_lower_bools(dict):
+    for key, value in dict.items():
+        if type(value) is bool:
+            value = str(value).lower()
+            dict[key] = value
+    return dict
+
+
+def open_json(json_file):
+    return json.load(open(json_file))
+
+
+def generate_diff(first_file, second_file, format=None):
+    file1 = make_lower_bools(open_json(first_file))
+    file2 = make_lower_bools(open_json(second_file))
+    keys1, keys2 = file1.keys(), file2.keys()
+    keys = sorted(keys1 | keys2)
+    result = ['{']
+    for key in keys:
+        if key in keys1 and key in keys2:
+            if file1[key] == file2[key]:
+                result.append('    {}: {}'.format(key, file1[key]))
+            else:
+                result.append('  - {}: {}'.format(key, file1[key]))
+                result.append('  + {}: {}'.format(key, file2[key]))
+        elif key not in keys1:
+            result.append('  + {}: {}'.format(key, file2[key]))
+        elif key not in keys2:
+            result.append('  - {}: {}'.format(key, file1[key]))
+    result.append('}')
+    result = '\n'.join(result)
+    print(result)
+
+
+if __name__ == "__main__":
+    pass
--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,15 +1,25 @@
 import argparse
 
 
-def main():
+from gendiff.generate_diff import generate_diff
+
+
+def parser_args():
     parser = argparse.ArgumentParser(
         prog='gendiff',
         description='Compares two configuration files and shows a difference.'
     )
-    parser.add_argument("first_file")
-    parser.add_argument("second_file")
+    parser.add_argument("first_file", type=str)
+    parser.add_argument("second_file", type=str)
     parser.add_argument("-f", "--format", help='set format of output')
     parser.parse_args()
+    args = parser.parse_args()
+    return args
+
+
+def main():
+    args = parser_args()
+    generate_diff(args.first_file, args.second_file)
 
 
 if __name__ == "__main__":
