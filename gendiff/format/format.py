33c9f0a16f2d1c64723cd970768e0c26369226a8
Maksim Vikhrov <maxvihr@yandex.ru>
2022-10-15 11:07:37 +0500

    Step8: Add json format

--- gendiff/format/format.py
+++ gendiff/format/format.py
@@ -1,5 +1,6 @@
 from gendiff.format.stylish import format_stylish
 from gendiff.format.plain import format_plain
+from gendiff.format.json import format_json
 
 
 def format_diff(tree, format):
@@ -7,5 +8,5 @@ def format_diff(tree, format):
         return format_stylish(tree)
     elif format == 'plain':
         return format_plain(tree)
-    else:
-        pass
+    elif format == 'json':
+        return format_json(tree)
