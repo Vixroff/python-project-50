49981d831ae55cb498d7e4a742eecad6c137fdfd
Maksim Vikhrov <maxvihr@yandex.ru>
2022-10-15 11:07:37 +0500

    Step8: Add json format

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -4,7 +4,8 @@ from gendiff.generate_diff import generate_diff
 
 def main():
     args = parser_args()
-    generate_diff(args.first_file, args.second_file, args.format)
+    result = generate_diff(args.first_file, args.second_file, args.format)
+    print(result)
 
 
 if __name__ == "__main__":
--- tests/test_generate_diff.py
+++ tests/test_generate_diff.py
@@ -1,5 +1,5 @@
 import pytest
-from gendiff.generate_diff import generate_diff
+from gendiff import generate_diff
 
 
 @pytest.mark.parametrize(
@@ -8,7 +8,8 @@ from gendiff.generate_diff import generate_diff
         ('tests/fixtures/file1_plain.json', 'tests/fixtures/file2_plain.json', 'stylish', 'tests/fixtures/json_plain_stylish_expected.txt'),
         ('tests/fixtures/file1_plain.yaml', 'tests/fixtures/file2_plain.yml', 'stylish', 'tests/fixtures/yaml_plain_stylish_expected.txt' ),
         ('tests/fixtures/file1_tree.json', 'tests/fixtures/file2_tree.json', 'stylish', 'tests/fixtures/json_tree_stylish_expected.txt'),
-        ('tests/fixtures/file1_tree.json', 'tests/fixtures/file2_tree.json', 'plain', 'tests/fixtures/json_tree_plain_expected.txt')
+        ('tests/fixtures/file1_tree.json', 'tests/fixtures/file2_tree.json', 'plain', 'tests/fixtures/json_tree_plain_expected.txt'),
+        ('tests/fixtures/file1_tree.json', 'tests/fixtures/file2_tree.json', 'json', 'tests/fixtures/json_tree_json_expected.txt')
     ]
 )
 
