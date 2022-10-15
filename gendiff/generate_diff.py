283fd002916901458cadd0c7581fa7f781f1214d
Maksim Vikhrov <maxvihr@yandex.ru>
2022-10-15 11:07:37 +0500

    Step8: Add json format

--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -3,11 +3,10 @@ from gendiff.tools.tree import get_diff_tree
 from gendiff.tools.reader import read
 
 
-def generate_diff(first_file, second_file, format):
+def generate_diff(first_file, second_file, format='stylish'):
     file1, file2 = read(first_file), read(second_file)
     diff = get_diff_tree(file1, file2)
     result = format_diff(diff, format)
-    print(result)
     return result
 
 
